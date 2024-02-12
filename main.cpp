0bfb0d12215b43af154b54f0f5878d89205e0d5a
Ignasiuzz <ignolaiskai@gmail.com>
2024-02-12 11:54:50 +0200

    Įvesties patvirtinimas
    Pridėtas mokinių ir namų darbų kiekio įvesties patvirtinimas

--- main.cpp
+++ main.cpp
@@ -1,35 +1,50 @@
 #include <iomanip>
 #include <iostream>
 #include <fstream>
+#include <limits>
 using namespace std;
 
-struct duom{
+struct duom {
     string vardas;
     string pavarde;
     int nd;
     int egz;
 };
 
-int main(){
-duom a[500];
-int n, m;
-cout<<"Iveskite mokiniu skaiciu";
-cin>>n;
-cout<<"Iveskite namu darbu skaiciu";
-cin>>m;
+int main() {
+    duom a[500];
+    int n, m;
 
-for (int i=0; i<n; i++){
-cout<<"Iveskite mokinio varda: ";
-cin>>a[i].vardas;
-cout<<"Iveskite mokinio pavarde: ";
-cin>>a[i].pavarde;
-    for (int j=0; j<m; j++){
-    cout<<"Iveskite mokinio namu darbu rezultata: ";
-    cin>>a[i].nd;
+    cout << "Iveskite mokiniu skaiciu: ";
+    cin >> n;
+    while (cin.fail()) { // tikrinama ar buvo ivestas skaicius, jei ne, prasoma ivesti vel
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Iveskite tinkama skaiciu!!!: ";
+        cin >> n;
     }
-cout<<"Iveskite mokinio egzamino rezultata: ";
-cin>>a[i].egz;
-}
 
+    cout << "Iveskite namu darbu skaiciu: ";
+    cin >> m;
+    while (cin.fail()) { // tikrinama ar buvo ivestas skaicius, jei ne, prasoma ivesti vel
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Iveskite tinkama skaiciu!!!: ";
+        cin >> m;
+    }
 
+    for (int i = 0; i < n; i++) { // informacijos ivedimas
+        cout << "Iveskite mokinio varda: ";
+        cin >> a[i].vardas;
+        cout << "Iveskite mokinio pavarde: ";
+        cin >> a[i].pavarde;
+        for (int j = 0; j < m; j++) {
+            cout << "Iveskite mokinio namu darbu rezultata: ";
+            cin >> a[i].nd;
+        }
+        cout << "Iveskite mokinio egzamino rezultata: ";
+        cin >> a[i].egz;
+    }
+
+    return 0;
 }
